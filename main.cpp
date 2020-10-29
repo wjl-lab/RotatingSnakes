9a8fb2c01ea5eb29056de9743fb1dcf013942d3b
wjl-lab <wjl15357374260@163.com>
2020-10-29 15:34:36 +0800

    画出扇形

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #define WIDTH 1200
 #define HEIGHT 800//初始画布宽高
 #define DIS 20//两个圆的距离
-#define RADIUS 180;//圆半径
+
 #define PI 3.14159
 
 int main()
@@ -16,15 +16,40 @@ int main()
 	cleardevice();
 
 	//定义圆的圆心
+	int RADIUS = 180;//圆半径
 	int centerX = DIS + RADIUS;
 	int centerY = DIS + RADIUS;
 
-	//画圆 
+
+	//画扇形 
 	for (centerX = 200; centerX < 1200; centerX = centerX + 400)
 	{
 		for (centerY = 200; centerY < 800; centerY = centerY + 400)
 		{
-			solidcircle(centerX, centerY, 180);
+			int i;
+			float offset;
+			
+			for (RADIUS = 200; RADIUS > 0; RADIUS = RADIUS - 50)
+			{
+				int left = centerX - RADIUS;
+				int right = centerX + RADIUS;
+				int top = centerY - RADIUS;
+				int bottom = centerY + RADIUS;
+
+				for (i = 0; i < 36; i++)
+				{
+					offset = i * PI / 18;
+					setfillcolor(RGB(255, 255, 0));
+					solidpie(left, top, right, bottom, offset, 1 * PI / 54 + offset);
+					setfillcolor(RGB(0, 0, 255));
+					solidpie(left, top, right, bottom, 1 * PI / 54 + offset, 2 * PI / 54 + offset);
+					setfillcolor(RGB(0, 255, 0));
+					solidpie(left, top, right, bottom, 2 * PI / 54 + offset, 3 * PI / 54 + offset);
+				}
+				
+			}
+
+
 		}
 	}
 	
