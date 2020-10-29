fa768ab67cd2995a468a52dce86d009841275c0b
wjl-lab <wjl15357374260@163.com>
2020-10-29 14:54:04 +0800

    画出6个小球

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <conio.h>
 #include <graphics.h>
 
-//初始画布宽高
+
 #define WIDTH 1200
-#define HEIGHT 800
+#define HEIGHT 800//初始画布宽高
+#define DIS 20//两个圆的距离
+#define RADIUS 180;//圆半径
 #define PI 3.14159
 
 int main()
@@ -13,8 +15,19 @@ int main()
 	setbkcolor(RGB(135, 127, 108));
 	cleardevice();
 
+	//定义圆的圆心
+	int centerX = DIS + RADIUS;
+	int centerY = DIS + RADIUS;
 
-
+	//画圆 
+	for (centerX = 200; centerX < 1200; centerX = centerX + 400)
+	{
+		for (centerY = 200; centerY < 800; centerY = centerY + 400)
+		{
+			solidcircle(centerX, centerY, 180);
+		}
+	}
+	
 	_getch();
 	return 0;
 }
