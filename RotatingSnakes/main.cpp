cfb210b1cb571927668e7a7eea7acc730e9da2c5
wjl-lab <wjl15357374260@163.com>
2020-10-29 14:26:49 +0800

    初始代码

---
+++ RotatingSnakes/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+
+//³õÊ¼»­²¼¿í¸ß
+#define WIDTH 1200
+#define HEIGHT 800
+#define PI 3.14159
+
+int main()
+{
+	initgraph(WIDTH, HEIGHT);
+	setbkcolor(RGB(135, 127, 108));
+	cleardevice();
+
+
+
+	_getch();
+	return 0;
+}
